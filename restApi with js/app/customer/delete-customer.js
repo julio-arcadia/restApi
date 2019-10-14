4e74d77198c333a4dc1c3063e9501785ea914ffa
arli1989 <56384757+arli1989@users.noreply.github.com>
2019-10-14 10:34:28 +0200

    Add files via upload

--- restApi with js/app/customer/delete-customer.js
+++ restApi with js/app/customer/delete-customer.js
@@ -1,7 +1,43 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function(){
+	//Se ejecuta si el botón delete fue clicado
+	$(document).on("click",".delete-customer-button", function(){
+		// Obtenemos la id del customer
+		var customer_id = $(this).attr("data-id");
+		// bootbox "confirm pop up"
+		bootbox.confirm({
+			message: "<h4>¿Estás seguro?</h4>",
+			buttons: {
+				confirm: {
+					label: "<span class='glyphicon glyphicon-ok'></span> Si",
+					className: "btn-primary"
+				},
+				cancel: {
+					label: "<span class='glyphicon glyphicon-remove'></span> No",
+					classname: "btn-primary"
+				}
+			},
+			callback: function (result){
+				if(result==true){
+					// Enviamos la consulta a la API
+					$.ajax({
+						url: "http://localhost/api/customer/delete.php",
+						type: "POST",
+						dataType: "json",
+						data: JSON.stringify({ id: customer_id}),
+						succes : function(result){
+							// recargamos lista de customers
+							showCustomers();		
+						},
+						error: function(xhr, resp, text){
+							console.log(xhr, resp, text);
+						}
+					});
+					
+				}
+			}			
+		});
+	});
+	
+});
 
 
